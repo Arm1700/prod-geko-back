1c60a4487a17afd1c0196ef0fcc27cdc2af56089
Arm1700 <bukbok1@gmail.com>
2024-11-26 14:49:46 +0400

    'restore'

--- main/management/commands/restore.py
+++ main/management/commands/restore.py
@@ -4,14 +4,16 @@ import re
 import environ
 from datetime import datetime
 from django.core.management import BaseCommand
+import shutil
 
 # Инициализация environ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+
 environ.Env.read_env()
 
 # Инициализация переменных окружения
 env = environ.Env()
-
 
 class Command(BaseCommand):
     help = 'Restore database and media files from a backup'
@@ -56,6 +58,59 @@ class Command(BaseCommand):
         db_backup_file = os.path.join(backup_folder, "db_backup.sql")
         media_backup_file = os.path.join(backup_folder, "media_backup.tar.gz")
 
+        # Удаление активных сессий из базы данных перед её удалением
+        self.stdout.write("Disconnecting all active sessions from the database...")
+        try:
+            subprocess.run(
+                [
+                    "psql", "-U", env('DB_USER'), "-h", env('DB_HOST'), "-p", env('DB_PORT'),
+                    "-c", f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{env('DB_NAME')}' AND pid <> pg_backend_pid();"
+                ],
+                check=True,
+                env={**os.environ, "PGPASSWORD": env('DB_PASSWORD')}
+            )
+        except subprocess.CalledProcessError as e:
+            self.stdout.write(f"Error disconnecting sessions: {e}")
+            return
+
+        # Удаление базы данных
+        self.stdout.write("Dropping existing database...")
+        try:
+            subprocess.run(
+                ["psql", "-U", env('DB_USER'), "-h", env('DB_HOST'), "-p", env('DB_PORT'), "-c", f"DROP DATABASE IF EXISTS {env('DB_NAME')}"],
+                check=True,
+                env={**os.environ, "PGPASSWORD": env('DB_PASSWORD')}
+            )
+        except subprocess.CalledProcessError as e:
+            self.stdout.write(f"Error during database deletion: {e}")
+            return
+
+        # Создание новой базы данных
+        self.stdout.write(f"Creating database {env('DB_NAME')}...")
+        try:
+            subprocess.run(
+                ["psql", "-U", env('DB_USER'), "-h", env('DB_HOST'), "-p", env('DB_PORT'), "-c", f"CREATE DATABASE {env('DB_NAME')}"],
+                check=True,
+                env={**os.environ, "PGPASSWORD": env('DB_PASSWORD')}
+            )
+        except subprocess.CalledProcessError as e:
+            self.stdout.write(f"Error during database creation: {e}")
+            return
+
+        # Удаление медиа файлов
+        self.stdout.write("Deleting media files...")
+        media_dir = os.path.join(BASE_DIR, 'media')
+        try:
+            # Удаляем все файлы и папки в директории media
+            for root, dirs, files in os.walk(media_dir, topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    shutil.rmtree(os.path.join(root, name))
+        except Exception as e:
+            self.stdout.write(f"Error during media files deletion: {e}")
+            return
+
         # Восстановление базы данных
         self.stdout.write(f"Restoring database from {db_backup_file}...")
         try:
@@ -74,7 +129,7 @@ class Command(BaseCommand):
         self.stdout.write(f"Restoring media files from {media_backup_file}...")
         try:
             subprocess.run(
-                ["tar", "-xzvf", media_backup_file, "-C", os.path.join(BASE_DIR, 'media')],
+                ["tar", "-xzvf", media_backup_file, "-C", media_dir],
                 check=True
             )
         except subprocess.CalledProcessError as e:
