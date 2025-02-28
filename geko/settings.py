e8ad9e19f082cdabdf2fbe4c5ad4ec6ab02bb8af
Arm1700 <bukboks1@gmail.com>
2025-02-28 17:58:03 +0400

    'static'

--- geko/settings.py
+++ geko/settings.py
@@ -235,7 +235,7 @@ LOCALE_PATHS = [
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
