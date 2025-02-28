7322eef8aaa09630c9092d4797478cc062bb6828
Arm1700 <bukboks1@gmail.com>
2025-02-28 17:03:16 +0400

    'static'

--- geko/settings.py
+++ geko/settings.py
@@ -244,14 +244,13 @@ LOCALE_PATHS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 STATIC_URL = '/django_static/'
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
